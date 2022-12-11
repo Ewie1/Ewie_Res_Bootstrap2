bee687a754baaaa2810b7e50e0feeddd10db925a
Ewie1 <shephvo12@gmail.com>
2022-12-11 11:37:41 +0000

    Add google map to interests page

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,4 +6,19 @@ function initMap () {
             lng: -33.134766
         }
     });
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    var locations = [
+        {lat: 40.785091, lng: -73.968285},
+        {lat: 41.084045, lng: -73.874345},
+        {lat: 40.754932, lng: -73.984016},
+    ];
+    var markers = location.map(function(location, i){
+        return new google.maps.Marker({
+            position: location,
+            label: label[i % labels.length]
+        });
+    });
+    var markerCluster = new markerClusterer(maps, markers, 
+        {imagePath: })
 }
